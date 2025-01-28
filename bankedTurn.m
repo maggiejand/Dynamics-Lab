370c4451e38dca23237226ef5b00bce391f9dd4b
jacobonthecorn <jale9668@colorado.edu>
2025-01-27 21:26:56 -0700

    First Attempt at Banked Turn
    Will upload a more complete version later this week (also got code back, yay!)

---
+++ bankedTurn.m
@@ -0,0 +1,19 @@
+% function [] = bankedTurn(h,theta)
+% Constants
+h = 100;
+theta = 0:90;
+g = 9.81;
+h_0 = 125;
+v = sqrt(2*g*(h_0-h));
+% Radius Calculation
+r = cotd(theta)*v^2/(g);
+hold on;
+plot(theta,r);
+xlabel('Angle of Bank (deg)')
+ylabel('Radius Needed (m)')
+% Normal G calculation
+G = 1./cosd(theta);
+yyaxis right
+ylabel('Gs Felt')
+xline(acosd(1/6)); % All angles past this are unusable (Normal Gs > 6)
+plot(theta,G)
