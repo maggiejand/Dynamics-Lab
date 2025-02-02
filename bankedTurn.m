3b5c42fc95758739ba789866958c4b385ea5d227
jacobonthecorn <jale9668@colorado.edu>
2025-02-01 22:39:37 -0700

    Slight Updates
    Will need to do a little more here, but it's still a start.

--- bankedTurn.m
+++ bankedTurn.m
@@ -1,12 +1,16 @@
-% function [] = bankedTurn(h,theta)
+function [G,x,y,z,s,vx,vy,vz] = bankedTurn(x0,y0,z0,vx0,vy0,vz0,theta)
+% banked turn: function inputs [initial position (x,y,z), radius, degree turn, initial velocity vectors] 
+% function outputs [G vectors, position vectors, path length, final velocity vectors]
 % Constants
-h = 100;
-theta = 0:90;
+% h = 100;
+% theta = 0:90;
 g = 9.81;
 h_0 = 125;
-v = sqrt(2*g*(h_0-h));
+v = sqrt(2*g*(h_0-z0));
 % Radius Calculation
 r = cotd(theta)*v^2/(g);
+s = r*theta;
+
 hold on;
 plot(theta,r);
 xlabel('Angle of Bank (deg)')
@@ -17,3 +21,5 @@ yyaxis right
 ylabel('Gs Felt')
 xline(acosd(1/6)); % All angles past this are unusable (Normal Gs > 6)
 plot(theta,G)
+
+vz = vz0;
