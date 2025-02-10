64d14961a7d6609e75c752226a44102ccf041752
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2025-02-10 14:45:24 -0700

    Update loopgs.m

--- loopgs.m
+++ loopgs.m
@@ -7,7 +7,7 @@ function [gforce_up, gforce_forward, vf] = loopgs(v0, r, s)
     g = 9.8;
     %v0 = sqrt(2*g*h0);
     v = sqrt(v0^2 - 2*g*sin((s/r) - pi/2));  % velocity along any given point of the loop
-    a_tang = -g*sin(s/r)/s; % derived equation for a tangential found using energy
+    a_tang = -g*sin(s/r); % derived equation for a tangential found using energy
     gforce_forward = a_tang/g; % gs felt due to normal force from the seat
     gforce_up = v0^2/(g*r) + 3*cos(s/r) - 2; % gs felt due to normal force at a given arc position s 
 end
