d83d4d65603efe45066f374fb27445f18a9b083c
maggiejand <maan9569@colorado.edu>
2025-02-10 10:26:11 -0700

    position is in function

--- bankedTurn.m
+++ bankedTurn.m
@@ -1,11 +1,9 @@
-function [G,x,y,z,s,vx,vy,vz] = bankedTurn(x0,y0,z0,vx0,vy0,vz0,phi,theta,theta_up)
+function [G,x,y,z,s,vx,vy,vz] = bankedTurn(x0,y0,z0,vx0,vy0,vz0,phi,theta)
 % banked turn: function inputs [initial position (x,y,z), initial velocity, degree bank, degree turn, initial velocity vectors] 
 % function outputs [G vectors, position vectors, path length, final velocity vectors]
 % Constants
 % h = 100;
-% Assumption: No initial z velocity and only one component of initial
-% velocity between x and y.
-theta_up = 0:theta_up;
+
 g = 9.81;
 h_0 = 125;
 v = sqrt(2*g*(h_0-z0));
@@ -13,28 +11,20 @@ v = sqrt(2*g*(h_0-z0));
 r = cotd(phi)*v^2/(g);
 s = r*theta;
 theta = 0:theta;
-if vy0 ~= 0 && vx0 == 0
-    center = [(x0+r) y0 z0];
-    z = center(3) + r*sind(theta_up);
-    x = center(1) - r*cosd(theta);
-    y = center(2) + r*sind(theta);
-    vx = r*sind(theta);
-    vy = r*cosd(theta);
-    vz = 0;
 
-elseif vx0 ~= 0 && vy0 == 0
-    center = [(x0) (y0+r) z0];
-    z = center(3) + r*sind(theta_up);
-    x = center(1) + r*sind(theta);
-    y = center(2) - r*cosd(theta);
-    vx = r*cosd(theta);
-    vy = r*sind(theta);
-    vz = 0;
+angle = atan(vy0/vx0);
+centerangled = [(0+r) 0 z0];
+directionp = centerangled(1)-r*cosd(theta);
+directionperp = center(2) + r*sind(theta);
 
-else
-    disp('Breaks Assumptions')
+x = (directionp * cos(angle) - directionperp * sin(angle))+x0;
+y = (directionp * sin(angle) + directionperp * cos(angle))+y0;
+z = centerangled(3)*ones(1,length(theta));
 
-end
+vz = vz0;
+vx = 5;
+vy=6;
+
 G = 1./cosd(phi);
 % hold on;
 plot(theta,G);
@@ -45,4 +35,3 @@ xlabel('Angle of Bank (deg)')
 % yyaxis right
 ylabel('Gs Felt')
 xline(acosd(1/6)); % All angles past this are unusable (Normal Gs > 6)
-
