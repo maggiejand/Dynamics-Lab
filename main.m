1a08482ac0fa58a3d37181b256f73f78e93ff2c9
maggiejand <maan9569@colorado.edu>
2025-02-10 10:11:43 -0700

    positioooon

--- main.m
+++ main.m
@@ -22,12 +22,26 @@ yp=directionp.*sin(angle);
 %% Example for Position of banked turn
 r1 = 50;
 theta1 = 0:45;
-center1 = [(100+r) 100 100];
-direction1p = center1(1)-r1*cosd(theta1); 
-direction2p = center(2)+r1*sind(theta1);
-x1p = direction1p.*cos(angle)+direction2p.*cos(angle);
-y1p = direction2p.*sin(angle)+direction1p.*sin(angle);
-z1p = center(3)*ones(1,length(theta1));
+angle1 = atan(vy/vx);
+
+centerangled = [(0+r) 0 0];
+direction1p = centerangled(1)-r*cosd(theta1);
+directionperp1 = center(2) + r*sind(theta1);
+
+x1p = direction1p * cos(angle1) - directionperp1 * sin(angle1);
+y1p = direction1p * sin(angle1) + directionperp1 * cos(angle1);
+z1p = centerangled(3)*ones(1,length(theta1));
+
+%% Position out of parabola
+angle2 = atan(vx/vy);
+
+initial = [0 0 0];
+height = 10;
+direction2p = linspace(0,2*sqrt(height),100);
+
+x2p = direction2p.*cos(angle2)+initial(1);
+y2p = direction2p.*sin(angle2)+initial(2);
+z2p = -(direction2p-sqrt(height)).^2+height+initial(3);
 
 %% Plotting 
 % no need to attatch position vectors, they don't need to be plotted with
@@ -35,7 +49,7 @@ z1p = center(3)*ones(1,length(theta1));
 
 
 figure (1)
-plot3(x1p,y1p,z1p);
+plot3(x2p,y2p,z2p);
 xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Z-axis');
